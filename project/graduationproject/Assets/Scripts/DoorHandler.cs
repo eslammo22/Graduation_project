6c33141ce9eea90910e51209b7b3943a8cd339d3
Abdulrahman-Hejazi <54711082+Abdulrahman-Hejazi@users.noreply.github.com>
2020-03-19 14:55:57 +0200

    add probuilder, progrides, and some uncompleted scripts

---
+++ project/graduationproject/Assets/Scripts/DoorHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorHandler : MonoBehaviour
+{
+
+    public GameObject doorHandler;
+    public bool drag;
+
+
+    private Animator anime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anime = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            if (drag)
+            {
+
+            }
+            else
+            {
+
+            }
+
+
+
+
+        }
+    }
+
+
+
+}
