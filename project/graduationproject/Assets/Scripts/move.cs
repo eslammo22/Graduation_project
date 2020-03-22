1b24d404cc2cbe39705251730e21fb11960fe4f5
eslammo22 <54930215+eslammo22@users.noreply.github.com>
2020-03-23 00:07:48 +0200

    frist v1
    my role

---
+++ project/graduationproject/Assets/Scripts/move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float speed = 6f;
+    public float gravity = 20f;
+    public float jump = 5f;
+    CharacterController controller;
+    Vector3 movedirection = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(controller.isGrounded)
+        {
+            movedirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+            movedirection *= speed;
+            if (Input.GetButton("jump"))
+            {
+                movedirection.y -= jump;
+
+            }
+        }
+        movedirection.y -= gravity * Time.deltaTime;
+        controller.Move(movedirection * Time.deltaTime); 
+    }
+}
